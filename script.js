667eef67d7262a4fcaedc102ee288fde858a181a
juliofl82 <151486186+juliofl82@users.noreply.github.com>
2023-11-30 17:08:10 -0300

    Entrega_Final

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 /* Desenvolva seu código abaixo */
-console.log('Testando github');
+function playRockPaperScissor(player1, player2) {
+    const pedra = 'Pedra';
+    const tesoura = 'Tesoura';
+    const papel = 'Papel';
+
+
+    if (player1 == pedra && player2 == tesoura) {
+        return 'Jogador 1 venceu!';
+    } else if (player1 == tesoura && player2 == papel) {
+        return 'Jogador 1 venceu!';
+    } else if (player1 == papel && player2 == pedra) {
+        return 'Jogador 1 venceu!';
+    } else if (player1 === player2) {
+        return 'Empate!';
+    } else {
+        return 'Jogador 2 venceu!';
+    }
+}
+
+console.log(playRockPaperScissor(player1, player2));
