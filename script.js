fb5965c66ad34298a5318330beab0354efa5c031
juliofl82 <151486186+juliofl82@users.noreply.github.com>
2023-12-05 18:57:39 -0300

    Incluido operador "or"

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ function playRockPaperScissor(player1, player2) {
     const pedra = 'Pedra';
     const tesoura = 'Tesoura';
     const papel = 'Papel';
-
-
-    if (player1 == pedra && player2 == tesoura) {
-        return 'Jogador 1 venceu!';
-    } else if (player1 == tesoura && player2 == papel) {
-        return 'Jogador 1 venceu!';
-    } else if (player1 == papel && player2 == pedra) {
+    
+    if ((player1 == pedra && player2 == tesoura) || (player1 == tesoura && player2 == papel) || (player1 == papel && player2 == pedra)) {
         return 'Jogador 1 venceu!';
     } else if (player1 === player2) {
         return 'Empate!';
@@ -17,5 +12,4 @@ function playRockPaperScissor(player1, player2) {
         return 'Jogador 2 venceu!';
     }
 }
-
 console.log(playRockPaperScissor(player1, player2));
